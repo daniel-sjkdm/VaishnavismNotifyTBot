4d23b3c4b20168468cf4e1abc4ac71dfcf9179dc
daniel-sjkdm <daniel.py.216@gmail.com>
2020-10-04 15:44:41 -0500

    constants

---
+++ helpers/helpers.py
@@ -0,0 +1,33 @@
+import re
+
+MONTH_TO_NUMBER = {
+    "January": 1,
+    "February": 2,
+    "March": 3,
+    "April": 4,
+    "May": 5,
+    "June": 6,
+    "July": 7,
+    "August": 8,
+    "September": 9,
+    "October": 10,
+    "November": 11,
+    "December": 12
+}
+
+NUMBER_TO_MONTH = dict(map(lambda x, y: (y, x), MONTH_TO_NUMBER.keys(), MONTH_TO_NUMBER.values()))
+
+DAY_TO_NUMBER = {
+    "Monday": 1,
+    "Tuesday": 2,
+    "Wednesday": 3,
+    "Thursday": 4,
+    "Friday": 5,
+    "Saturday": 6,
+    "Sunday": 7
+}
+
+NUMBER_TO_DAY = dict(map(lambda x, y: (y, x), DAY_TO_NUMBER.keys(), DAY_TO_NUMBER.values()))
+
+
+DATE_PATTERN = re.compile("((1?[012]|[1-9])(-\d{4})?|(\d{4})(-(1?[012]|[1-9]))?)")
