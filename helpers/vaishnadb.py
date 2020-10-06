ae215735015312cd375cf3b3fcfb608a0801a7a1
daniel-sjkdm <daniel.py.216@gmail.com>
2020-10-06 13:13:16 -0500

    Postgresql class connection

---
+++ helpers/vaishnadb.py
@@ -0,0 +1,49 @@
+import os
+import psycopg2
+
+
+class VaishnaDB():
+    def __init__(self):
+        self.dbname = dbname=os.getenv('PGDATABASE'),
+        self.host = os.getenv('PGHOST'),
+        self.user = os.getenv('PGUSER'),
+        self.password = os.getenv('PGPASSWORD'),
+        self.port = os.getenv('PGPORT')
+    
+
+    def get_db_connection(self):
+        return psycopg2.connect(
+            dbname=self.dbname,
+            host=self.host,
+            user=self.user,
+            password=self.password,
+            port=self.port
+        )
+
+
+    def get_iskcon_events(self, data, fetch_by):
+        with self.get_db_connection() as conn:
+            with conn.cursor() as cursor:
+                if fetch_by == "year":
+                    cursor.execute("SELECT * FROM iskcon_events WHERE year=%s", (data,))
+                    return cursor.fetchall()
+                elif fetch_by == "month":
+                    cursor.execute("SELECT * FROM iskcon_events WHERE month=%s", (data,))
+                    return cursor.fetchall()
+                else:
+                    cursor.execute("SELECT * FROM iskcon_events WHERE month=%s AND year =%s", (data[0], data[1]))
+                    return cursor.fetchall()
+                
+
+    def get_ekadasi_events(self, data, fetch_by):
+        with self.get_db_connection() as conn:
+            with conn as cursor:
+                if fetch_by == "year":
+                    cursor.execute("SELECT * FROM ekadasi_events WHERE year=%s", (data,))
+                    return cursor.fetchall()
+                elif fetch_by == "month":
+                    cursor.execute("SELECT * FROM ekadasi_events WHERE month=%s", (data,))
+                    return cursor.fetchall()
+                else:
+                    cursor.execute("SELECT * FROM ekadasi_events WHERE month=%s AND year =%s", (data[0], data[1]))
+                    return cursor.fetchall()
