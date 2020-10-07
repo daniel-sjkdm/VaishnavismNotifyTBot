12ed46dfcf42c92a3acebb0f7152d05d6e9a1ba1
daniel-sjkdm <daniel.py.216@gmail.com>
2020-10-06 19:15:44 -0500

    cleanin

--- helpers/vaishnadb.py
+++ helpers/vaishnadb.py
@@ -14,8 +14,6 @@ class VaishnaDBPG():
 
     def get_db_connection(self):
 
-        print("Requested a connection to the database")
-
         connection = psycopg2.connect(
             dbname=self.dbname,
             host=self.host,
@@ -24,49 +22,32 @@ class VaishnaDBPG():
             port=self.port
         )
 
-        print("Connection was created, not sure if valid...")
-        print(f"Connection: {connection}")
-
         return connection
 
 
     def get_iskcon_events(self, data, fetch_by):
-        print("1. Fetch iskcon events")
-        conn = self.get_db_connection()
-        print("1.2 Connection = ", conn.status)
-        with conn.cursor() as cursor:
-            print("2. Cursor = ", cursor)
+        with self.get_db_connection() as conn:
+            cursor = conn.cursor()
             if fetch_by == "year":
                 cursor.execute("SELECT * FROM iskcon_events WHERE year=%s", (data,))
-                events = cursor.fetchall()
             elif fetch_by == "month":
                 cursor.execute("SELECT * FROM iskcon_events WHERE month=%s", (data,))
-                events = cursor.fetchall()
             else:
                 cursor.execute("SELECT * FROM iskcon_events WHERE month=%s AND year=%s", (data[0], data[1]))
-                events = cursor.fetchall()
-            print(events)
-        conn.close()
-        return events
+            events = cursor.fetchall()
+            return events
 
 
     def get_ekadasi_events(self, data, fetch_by):
-        print("1. Fetch ekadasi events")
-        conn = self.get_db_connection()
-        print("2. Connection = ", conn.status)
-        with conn as cursor:
-            print("3. Cursor = ", cursor)
+        with self.get_db_connection() as conn:
+            cursor = conn.cursor()
             if fetch_by == "year":
                 cursor.execute("SELECT * FROM ekadasi_events WHERE year=%s", (data,))
-                events = cursor.fetchall()
             elif fetch_by == "month":
                 cursor.execute("SELECT * FROM ekadasi_events WHERE month=%s", (data,))
-                events = cursor.fetchall()
             else:
                 cursor.execute("SELECT * FROM ekadasi_events WHERE month=%s AND year=%s", (data[0], data[1]))
-                events = cursor.fetchall()
-            print(events)
-        conn.close()
+        events = cursor.fetchall()
         return events
 
 
