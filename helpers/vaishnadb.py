92a853cdfc89b656dc2b43630b5ff43f20b2df22
daniel-sjkdm <daniel.py.216@gmail.com>
2020-10-08 18:46:08 -0500

    The psql connection was fixed and is now working

--- helpers/vaishnadb.py
+++ helpers/vaishnadb.py
@@ -1,24 +1,31 @@
 import os
 import psycopg2
+from dotenv import load_dotenv
 
+load_dotenv(dotenv_path="bot/.env")
 
-class VaishnaDB():
-    def __init__(self):
-        self.dbname = dbname=os.getenv('PGDATABASE'),
-        self.host = os.getenv('PGHOST'),
-        self.user = os.getenv('PGUSER'),
-        self.password = os.getenv('PGPASSWORD'),
-        self.port = os.getenv('PGPORT')
+class VaishnaDBPG():
+
+    dbname = os.getenv('PGDATABASE')
+    host = os.getenv('PGHOST')
+    user = os.getenv('PGUSER')
+    password = os.getenv('PGPASSWORD')
+    port = os.getenv('PGPORT')
     
 
     def get_db_connection(self):
-        return psycopg2.connect(
+        conn = psycopg2.connect(
             dbname=self.dbname,
             host=self.host,
             user=self.user,
             password=self.password,
             port=self.port
         )
+
+        if conn.status == 1:
+            return conn
+        else:
+            raise Exception("Connection failed")
 
 
     def get_iskcon_events(self, data, fetch_by):
@@ -37,7 +44,7 @@ class VaishnaDB():
 
     def get_ekadasi_events(self, data, fetch_by):
         with self.get_db_connection() as conn:
-            with conn as cursor:
+            with conn.cursor() as cursor:
                 if fetch_by == "year":
                     cursor.execute("SELECT * FROM ekadasi_events WHERE year=%s", (data,))
                     return cursor.fetchall()
