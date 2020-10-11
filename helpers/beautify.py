cbe72a5ead001a95cff3bbfbb2704c65a41e182b
daniel-sjkdm <daniel.py.216@gmail.com>
2020-10-11 14:14:02 -0500

    Beautify the html and export to pdf

--- helpers/beautify.py
+++ helpers/beautify.py
@@ -1,23 +1,37 @@
 from jinja2 import Environment, FileSystemLoader, PackageLoader, select_autoescape
-from xhtml import pisa
 from io import BufferedReader, BytesIO
+from settings import BASE_DIR
+import os.path
+import pdfkit 
 
+
+template_path = os.path.join(BASE_DIR, "beautify/templates")
 
 env = Environment(
-    loader=FileSystemLoader("templates"),
+    loader=FileSystemLoader(template_path),
     autoescape=select_autoescape(["html", "xml"]),
-    enable_async=True
 )
 
 
-def beautify_events(events, kind):
+def beautify_to_pdf(data, kind):
+    template = env.get_template("ekadasi_events.html")
+    css = [
+        os.path.join(BASE_DIR, "beautify/static/css/bootstrap.css")   
+    ]
+
     if kind == "ekadasi":
         template = env.get_template("ekadasi_events.html")
+        css.append(os.path.join(BASE_DIR, "beautify/static/css/ekadasi_events.css"))
+
     elif kind == "iskcon_events":
         template = env.get_template("iskcon_events.html")
+        css.append(os.path.join(BASE_DIR, "beautify/static/css/iskcon_events.css"))
+
     html = template.render(
-        events = events
+        events=data["events"],
+        year=data["year"],
+        month=data["month"]
     )
-    pdffile_bytes = BytesIO()
-    pisa.CreatePDF(src=html, dest=pdffile_bytes)
-    return BufferedReader(BytesIO(pddfile_bytes.getvalue()))
+
+    pdffile_bytes = BytesIO(pdfkit.from_string(html, output_path=False, css=css))
+    return BufferedReader(BytesIO(pdffile_bytes.getvalue()))
